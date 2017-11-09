04989102a687c91f3d35298e0be20f28a11ffc7d
Joao Neves <joaoneves792@gmail.com>
2017-11-09 21:39:26 +0000

    Added parseGLError

--- projects/shader/src/glerror.cpp
+++ projects/shader/src/glerror.cpp
@@ -1,16 +1,32 @@
 #include <GL/glew.h>
 #include <string>
 #include <iostream>
-#include "GL/glut.h"
 #include "glerrors.h"
+
+std::string parseGLError(int error){
+    switch (error) {
+        case GL_INVALID_OPERATION:
+            return "GL_INVALID_OPERATION";
+        case GL_INVALID_ENUM:
+            return "GL_INVALID_ENUM";
+        case GL_INVALID_VALUE:
+            return "GL_INVALID_VALUE";
+        case GL_INVALID_FRAMEBUFFER_OPERATION:
+            return "GL_INVALID_FRAMEBUFFER_OPERATION";
+        case GL_OUT_OF_MEMORY:
+            return "GL_OUT_OF_MEMORY";
+        default:
+            return "Unknown " + error;
+    }
+}
 
 bool isOpenGLError() {
         bool isError = false;
         GLenum errCode;
-        const GLubyte *errString;
+        std::string errString;
         while ((errCode = glGetError()) != GL_NO_ERROR) {
                 isError = true;
-                errString = gluErrorString(errCode);
+                errString = parseGLError(errCode);
                 std::cerr << "OpenGL ERROR [" << errString << "]." << std::endl;
         }
         return isError;
